739ca319d59b411503d508e0a8463e17ed6b1722
Jen Merritt <jenjomez@gmail.com>
2019-11-27 11:34:00 +0000

    pub class setup and getter methods tests passed

---
+++ specs/pub_spec.rb
@@ -0,0 +1,33 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../pub')
+require_relative('../customer')
+require_relative('../drink')
+
+class PubTest < MiniTest::Test
+
+  def setup
+
+    @drink1 = Drink.new("Beer", 4)
+    @drink2 = Drink.new("Wine", 1)
+    @drink3 = Drink.new("Spirit", 5)
+
+    @customer1 = Customer.new("Andrew", 50)
+
+    @drinks = [@drink1, @drink2, @drink3]
+
+    @pub = Pub.new("The Southern", 1000, [@drinks])
+
+  end
+
+  def test_has_name
+    assert_equal("The Southern", @pub.name)
+  end
+
+  def test_amount_of_cash_in_till
+    assert_equal(1000, @pub.till)
+  end
+
+end
